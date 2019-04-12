60a4714f794804629b35aa724d09ed6999599a42
hanhehuan <1242017454@qq.com>
2019-04-12 09:12:19 +0800

    测试helloworld

---
+++ src/main/java/com/example/hhh/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.hhh.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public @ResponseBody String hello(){
+        return "hello world!";
+    }
+}
